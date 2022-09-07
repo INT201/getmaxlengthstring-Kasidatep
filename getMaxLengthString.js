b3554270848e9ca82d9dcffd516f16b2118d4f90
Kasidatep <bank@kasidate.me>
2022-09-07 14:52:01 +0700

    Kasidate

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,13 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here ------------------
+  let arr = arrayOfString
+  let strLength
+
+  for(let i = 0; i < arr.length ; i++)
+    let a = arr[i].length
+    if (a > strLength){
+      strLength = a
+    }
 }
 module.exports = getMaxLengthString
