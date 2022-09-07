e84e2276618084c67c286beba70b3e065926fc25
Kasidatep <bank@kasidate.me>
2022-09-07 18:13:06 +0700

    TEST#3

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,4 +1,4 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
   let maxLength = ['']
@@ -8,8 +8,7 @@ function getMaxLengthString(arrayOfString) {
   } else{
     for (let i = 0; i < arrayOfString.length; i++) {
       if(arrayOfString[i].length > maxLength[(maxLength.length-1)].length){
-        maxLength = []
-        maxLength.push(arrayOfString[i])
+        maxLength = [arrayOfString[i]]
       }else if(arrayOfString[i].length === maxLength[j].length){
         maxLength.push(arrayOfString[i])
       }
@@ -19,4 +18,17 @@ function getMaxLengthString(arrayOfString) {
   }
   
 }
-module.exports = getMaxLengthString
+//module.exports = getMaxLengthString
+
+getMaxLengthString(['hello', 'hi', 'hey', 'Good Evening', 'Good Morning']) 
+getMaxLengthString(['hi']) 
+getMaxLengthString(null)
+getMaxLengthString(undefined) 
+console.log(getMaxLengthString(['hello', 'hi', 'hey']))
+console.log(getMaxLengthString(['hi']) )
+console.log(getMaxLengthString(['hello', 'hi', 'hey', 'Good Evening', 'Good Morning'])
+)
+console.log(getMaxLengthString(['hello', 'hi', 'hey', 'Good Evening', 'Good Morning', 'Gooddddddddddddddddddddd'])
+)
+console.log(getMaxLengthString(null))
+console.log(getMaxLengthString(undefined))
